1235ac424d11fe09b512f2c6df8ed960d205a51b
Neylin <n00199817@upn.pe>
2026-06-04 15:12:50 -0500

    Agregar y modifiar para el juego5 y constructor tipo poo

---
+++ Game5/PokeDemo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game5
+{
+    public class PokeDemo
+    {
+        private Arbol5 arbol = new Arbol5();
+
+        
+    }
+}
