9e3116199cc81c4723f04b56c297a973a854524f
Ander14C <andersoncruzadoalvarez64@gmail.com>
2026-06-05 12:10:07 -0500

    Metodos de la clase Arbol LISTO👍

--- Game4/Arbol4.cs
+++ Game4/Arbol4.cs
@@ -8,7 +8,38 @@ namespace Game4
 {
     public class Arbol4
     {
+        public Nodo raiz = null;
 
+        //Crea y conecta todas las salas de la mazmorra, asignando enemigos y oro a cada sala
+        public void CrearMazmorra()
+        {
 
+        }
+
+        // Devuelve el nombre de la sala actual
+        public string Mostrarsala(Nodo sala)
+        {
+            return sala.nombSala;
+        }
+        //public Nodo IrIzquierda(Nodo actual)
+        //{
+
+        //}
+        //public Nodo IrDerecha(Nodo actual)
+        //{
+
+        //}
+
+        // Verifica si la sala actual tiene un enemigo
+        public bool VerificarEnemigo(Nodo sala)
+        {
+            return sala.enemigoSala != null;
+        }
+
+        // Verifica si la sala actual es una sala final (sin salas conectadas)
+        public bool EsFinal(Nodo sala)
+        {
+            return sala.izquierda == null && sala.derecha == null;
+        }
     }
 }
