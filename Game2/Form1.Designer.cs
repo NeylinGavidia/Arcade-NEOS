315dd844fe9c95b6165dd4e5f096b06243e129f1
Luz Marita <sluz3151@gmail.com>
2026-06-07 12:16:48 -0500

    finalizando Espero

--- Game2/Form1.Designer.cs
+++ Game2/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Game2
 {
-    partial class Form1
+    partial class FrmFnutivinanza
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.Font = new System.Drawing.Font("Arial Black", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(204, 28);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(301, 40);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "FRUTIVINANZA  ";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Arial Black", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(301, 40);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "FRUTIVINANZA  ";
+            // 
+            // FrmFnutivinanza
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(834, 651);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTitulo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FrmFnutivinanza";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FRUTIVINANZA";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label label1;
     }
 }
