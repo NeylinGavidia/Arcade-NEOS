31436f7ea635aa65a4fbc5b8697cbc98a35caf01
NatalySG27 <N00462738@upn.pe>
2026-06-09 21:33:50 -0500

    Fondo arreglado

--- Game1/Form1.Designer.cs
+++ Game1/Form1.Designer.cs
@@ -168,7 +168,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImage = global::Game1.Properties.Resources.Fondo;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(697, 430);
             this.Controls.Add(this.lblTitulo);
